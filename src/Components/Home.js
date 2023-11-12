080cad2ca1f38b883e2667dc5e6b74bfff6f3fd6
Matthew Luk <matthew.luk@yahoo.com>
2023-11-11 21:20:50 -0800

    update file names and styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,27 +3,32 @@ import axios from "axios";
 import Navbar from './Navbar';
 import '../css/styles.scss'
 import Card from './Card';
-import { convertDate } from './functions';
+import { convertDate, getSun } from './functions';
 
 const Home = () => {
     const [ip, setIP] = useState("")
     const [weatherList,setWeatherList] = useState([])
     const [currentWeather, setCurrentWeather] = useState({})
+    const [sunOut, setSunOut] = useState("")
 
     const getData = async () => {
         const res = await axios.get("https://api.ipify.org/?format=json");
         setIP(res.data.ip);
-        axios.get(`http://api.weatherapi.com/v1/current.json?key=ed7bf3890ad2432497a63148232608&q=${res.data.ip}&aqi=yes`)
+        // axios.get(`http://api.weatherapi.com/v1/current.json?key=ed7bf3890ad2432497a63148232608&q=${res.data.ip}&aqi=yes`)
+        // axios.get(`http://api.weatherapi.com/v1/forecast.json?key=ed7bf3890ad2432497a63148232608&q=${res.data.ip}&aqi=yes`)
+        axios.get(`http://api.weatherapi.com/v1/forecast.json?key=ed7bf3890ad2432497a63148232608&q=${res.data.ip}&days=3&aqi=no&alerts=no`)
         .then((result) => {
+            // console.log(result.data.forecast.forecastday[0].astro.is_sun_up)
             setCurrentWeather({
                 cityName: result.data.location.name,
-                countryName: result.data.location.country,
-                time: convertDate(result.data.location.localtime),
-                updated: convertDate(result.data.current.last_updated),
                 F: result.data.current.temp_f,
                 C: result.data.current.temp_c,
+                time: convertDate(result.data.location.localtime),
+                updated: convertDate(result.data.current.last_updated),
                 condition: result.data.current.condition.text,
-                icon: result.data.current.condition.icon
+                icon: result.data.current.condition.icon,
+                forecastList: result.data.forecast.forecastday,
+                sunOut: getSun(result.data.forecast.forecastday[0].astro.is_sun_up)
             })
         })
         .catch((err) => {
@@ -36,19 +41,30 @@ const Home = () => {
     },[])
 
     return (
-        <div className='container'>
+        <div className='container' style={{}}>
             <Navbar weatherList={weatherList} setWeatherList={setWeatherList}/>
             <div className='currentCard'>
-                <p>{currentWeather.cityName}</p>
-                <p>{currentWeather.countryName}</p>
-                <p>{currentWeather.time}</p>
-                <p>{currentWeather.updated}</p>
-                <div className='temperatures'>
-                    <p>{currentWeather.F}</p>
-                    <p>{currentWeather.C}</p>
+                <div className='currentCardHeader'>
+                    <h2>Your Location (based on IP address)</h2>
+                    <p>{currentWeather.cityName}</p>
+                    <div className='temperatures'>
+                        <p>{currentWeather.F}&deg;F</p>
+                        <p>{currentWeather.C}&deg;C</p>
+                    </div>
+                    <img alt='weather icon' src={currentWeather.icon}></img>
+                    <p>{currentWeather.condition}</p>
+                    <p>Local Time: {currentWeather.time}</p>
+                    <p>Updated at: {currentWeather.updated}</p>
+                    <p>{currentWeather.sunOut}</p>
                 </div>
-                <p>{currentWeather.condition}</p>
-                <img alt='weather icon' src={currentWeather.icon}></img>
+                <div className='currentCardContent'>
+                    <div className=''>
+
+                    </div>
+                    <div className='forecast'>
+
+                    </div>
+                </div>
             </div>
             <div>
                 <Card />
--- src/Components/functions.js
+++ src/Components/functions.js
@@ -1,4 +1,27 @@
 export function convertDate(date){
-    console.log(date)
-    return date
+    let str = ""
+    if(parseInt(date.slice(11,13)) > 12){
+        str = parseInt(date.slice(11,13) - 12).toString() + date.slice(13,16) + "PM"
+    }else{
+        if(date.slice(11,13) == "00"){
+            str = "12" + date.slice(13,16) + "AM"
+        }else if(date.slice(11,13) == "12"){
+            str = "12" + date.slice(13,16) + "PM"
+        }else{
+            if(parseInt(date.slice(11,13)) < 10){
+                str = date.slice(12,13) + date.slice(13,16) + "AM"
+            }else{
+                str = date.slice(11,13) + date.slice(13,16) + "AM"
+            }
+        }
+    }
+    return str
+}
+
+export function getSun(x){
+    if(x === 1){
+        return "backgroundImage: linearGradient(#49c0d4, #fae97f);"
+    }else{
+        return "backgroundImage: linearGradient(#1417b2, #52095d);"
+    }
 }
